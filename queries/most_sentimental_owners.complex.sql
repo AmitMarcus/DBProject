0f08d8ed1869d859ef35d27cf18f98c101ce96c5
Niv300000 <Niv300000@gmail.com>
2017-01-23 01:45:07 +0200

    Update most_sentimental_owners.complex.sql

--- queries/most_sentimental_owners.complex.sql
+++ queries/most_sentimental_owners.complex.sql
@@ -4,11 +4,12 @@ SELECT
 FROM
     Event,
     Owner,
-    Event_Owner
+    Event_Owner,
+    Event_Desc_Search
 WHERE
-    Event.id = Event_Owner.event_id
+    Event.id = Event_Owner.event_id AND Event.id=Event_Desc_Search.event_id
         AND Event_Owner.owner_id = Owner.id
-        AND (MATCH (Event.description) AGAINST ('+Love' IN BOOLEAN MODE))
+        AND (MATCH (Event_Desc_Search.description) AGAINST ('+Love' IN BOOLEAN MODE))
 GROUP BY Owner.id
-ORDER BY Number_Of_Events_With_Love DESC
+ORDER BY number_of_events DESC
 LIMIT 10;
