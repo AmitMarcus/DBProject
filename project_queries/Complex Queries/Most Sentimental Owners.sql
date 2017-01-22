c9f4d6362569dd4817f2c293a4fe2327026977b0
orperel <or_perel@hotmail.com>
2017-01-22 03:51:56 +0200

    refreshing queries

--- project_queries/Complex Queries/Most Sentimental Owners.sql
+++
@@ -1,6 +0,0 @@
-SELECT Owner.id AS Owner_ID, Owner.name AS Owner_Name,COUNT(Event.id) AS Number_Of_Events_With_Love
-FROM Event,Owner,Event_Owner
-WHERE Event.id=Event_Owner.event_id and Event_Owner.owner_id=Owner.id and (MATCH(Event.description) AGAINST('+Love' IN BOOLEAN MODE))
-GROUP BY Owner.id
-ORDER BY Number_Of_Events_With_Love DESC
-LIMIT 10;
--- project_queries/Queries With Inputs/Search details of event by id.sql
+++
@@ -1,6 +0,0 @@
-SELECT Event.id AS Event_ID,Event.name AS Event_Name, Category.name AS Category_Name,
-	 Country.name AS Country_Name,City.name AS City_Name, Street.name AS Street_Name,
-	 Event.description AS Event_Description
-FROM Event,Event_Place,Place,Country,City,Street,Category
-WHERE Event.id=id_input and Event.id=Event_Place.event_id and Event_Place.place_id = Place.id and Place.street_id=Street.id 
-								and Street.city_id=City.id and City.country_id=Country.id and Event.category_id=Category.id;
