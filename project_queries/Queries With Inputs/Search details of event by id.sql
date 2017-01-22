6f88c4973d352df62b00f09543c199aa758d67b7
Niv300000 <Niv300000@gmail.com>
2017-01-23 01:15:30 +0200

    Update Search details of event by id.sql

--- project_queries/Queries With Inputs/Search details of event by id.sql
+++ project_queries/Queries With Inputs/Search details of event by id.sql
@@ -1,6 +1,7 @@
 SELECT Event.id AS Event_ID,Event.name AS Event_Name, Category.name AS Category_Name,
 	 Country.name AS Country_Name,City.name AS City_Name, Street.name AS Street_Name,
-	 Event.description AS Event_Description
-FROM Event,Event_Place,Place,Country,City,Street,Category
+	 Event_Desc_Search.description AS Event_Description
+FROM Event,Event_Place,Place,Country,City,Street,Category,Event_Desc_Search
 WHERE Event.id=id_input and Event.id=Event_Place.event_id and Event_Place.place_id = Place.id and Place.street_id=Street.id 
-								and Street.city_id=City.id and City.country_id=Country.id and Event.category_id=Category.id;
+			and Street.city_id=City.id and City.country_id=Country.id and Event.category_id=Category.id
+			and Event.id=Event_Desc_Search.event_id;
