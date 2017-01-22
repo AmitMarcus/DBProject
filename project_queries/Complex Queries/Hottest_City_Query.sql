363894033eb4da9d30dba994dccc05a140cb084d
Niv300000 <Niv300000@gmail.com>
2017-01-23 01:57:35 +0200

    Update Hottest_City_Query.sql

--- project_queries/Complex Queries/Hottest_City_Query.sql
+++ project_queries/Complex Queries/Hottest_City_Query.sql
@@ -1,11 +1,12 @@
-SELECT City.name AS City_Name,Event.id AS Event_ID,Event.name AS Event_Name,Category.name AS Category_Name,Event.description AS Event_Description,DATE(Event_Time.start_time) AS EventStartDate,DATE(Event_Time.end_time) as EventEndDate
-FROM Event,Street,City,Category,Place,Event_Place,Event_Time,(SELECT City1.id AS CityId1 ,Count(Event1.id) As Num_Of_Events
+SELECT City.name AS City_Name,Event.id AS Event_ID,Event.name AS Event_Name,Category.name AS Category_Name,Event_Desc_Search.description AS Event_Description,DATE(Event_Time.start_time) AS EventStartDate,DATE(Event_Time.end_time) as EventEndDate
+FROM Event,Street,City,Category,Place,Event_Place,Event_Time,Event_Desc_Search,(SELECT City1.id AS CityId1 ,Count(Event1.id) As Num_Of_Events
 																					FROM Event AS Event1,Place AS Place1,Event_Place AS EventPlace1 ,Street AS Street1,City AS City1
 																					WHERE Place1.street_id = Street1.id and Street1.city_id=City1.id and Event1.id=EventPlace1.event_id and EventPlace1.place_id=Place1.id
 																					GROUP BY City1.id
 																					ORDER BY Num_Of_Events DESC
 																					LIMIT 1) AS CityMaxEventsTable
 WHERE Event.category_id=Category.id and Event.id=Event_Place.event_id and Event_Place.place_id=Place.id and Place.street_id=Street.id and Street.city_id = City.id 
-									and City.id=CityMaxEventsTable.CityId1 and Event.id=Event_Time.event_id and Event_Time.start_time>=CURDATE() 
+									and City.id=CityMaxEventsTable.CityId1 and Event.id=Event_Time.event_id and Event_Time.start_time>=CURDATE()
+									and Event.id=Event_Desc_Search.event_id
 ORDER BY (Event_Time.start_time)
 LIMIT 10;
