0fc690f1451bdcbe779d57834110d3e6d4707b18
orperel <or_perel@hotmail.com>
2017-01-24 01:18:01 +0200

    fb_update changed to times

--- fb_update.py
+++ fb_update.py
@@ -9,14 +9,13 @@ import facebook
 # Facebook app token
 app_token = '190441714759199|QdoMmLvHYEUzSeeLyPK4Awg6Zv4'
 
-def fetch_updated_event_guests_data(event_id):
+def fetch_updated_event_times_data(event_id):
 
     try:
         graph = facebook.GraphAPI(access_token=app_token, version='2.2')
 
         event = graph.get_objects(ids=[event_id],
-                                           fields='id, attending_count,declined_count,maybe_count,' +
-                                                  'noreply_count,interested_count')
+                                           fields='id, start_time, end_time, update time')
     except facebook.GraphAPIError:
         print ('Facebook-graph error fetching updated event data. Event_id: ' + str(event_id))
         tb = traceback.format_exc()
@@ -26,67 +25,57 @@ def fetch_updated_event_guests_data(event_id):
         return None, None, None, None, None
 
     event_data = event[event_id]
-    event_attending_count = event_data.get('attending_count', None)
-    event_declined_count = event_data.get('declined_count', None)
-    event_interested_count = event_data.get('interested_count', None)
-    event_maybe_count = event_data.get('maybe_count', None)
-    event_noreply_count = event_data.get('noreply_count', None)
+    event_start_time = event_data.get('start_time',
+                                 None)  # Time format is String here, convert to DB time format..
+    event_end_time = event_data.get('end_time', None)
+    event_updated_time = event_data.get('updated_time', None)
 
-    return event_attending_count, event_declined_count, event_interested_count, event_maybe_count, event_noreply_count
+    return event_start_time, event_end_time, event_updated_time
 
 
-def execute_update_query(conn, event_id, event_attending_count, event_declined_count, event_interested_count,\
-                         event_maybe_count, event_noreply_count):
+def execute_update_query(conn, event_id, event_start_time, event_end_time, event_updated_time):
 
-    guests_update_query = 'UPDATE Event_Guests ' \
-                          'SET attending_count=\'%s\', ' \
-                          'declined_count=\'%s\', ' \
-                          'maybe_count=\'%s\', ' \
-                          'interested_count=\'%s\', ' \
-                          'noreply_count=\'%s\' ' \
+    times_update_query = 'UPDATE Event_Time ' \
+                          'SET start_time=\'%s\', ' \
+                          'end_time=\'%s\', ' \
+                          'updated_time=\'%s\'
                           'WHERE event_id=\'%s\''
 
-    event_guests_parameters = (event_attending_count,
-                               event_declined_count,
-                               event_maybe_count,
-                               event_interested_count,
-                               event_noreply_count,
-                               long(event_id))
+    event_times_parameters = (str(event_start_time),
+                              str(event_end_time),
+                              str(event_updated_time),
+                              long(event_id))
 
     try:
         # Create cursor for executing UPDATE query..
         cur = conn.cursor(MySQLdb.cursors.DictCursor)
 
-        cur.execute(guests_update_query, event_guests_parameters)
+        cur.execute(times_update_query, event_times_parameters)
     except MySQLdb.IntegrityError:
-        print("Skipping duplicate Event-Guest entry: " + str(event_id))
+        print("Skipping duplicate Event-Time entry: " + str(event_id))
     except MySQLdb.OperationalError:
         pass  # Silently ignore constraint errors, this is the database protecting against bad FB records..
     except Exception:
         con.rollback()  # Any other exception should be rolled back to protect DB integrity
 
-def update_event_guests(conn, event_id):
-    """ Fetches the updated event-guests details using the facebook graph-api (number of attending, declined, etc),
-        and updates the event_guests entry at the Event-Guests table in the database accordingly.
+def update_event_times(conn, event_id):
+    """ Fetches the updated event-times details using the facebook graph-api (start, end, updated times),
+        and updates the event_times entry at the Event-Times table in the database accordingly.
         @:return True if the update process succeeded, False is failed
     """
     try:
-        event_attending_count, event_declined_count, event_interested_count,\
-        event_maybe_count, event_noreply_count = fetch_updated_event_guests_data(event_id)
+        event_start_time, event_end_time, event_updated_time = fetch_updated_event_times_data(event_id)
 
-        if (event_attending_count is None) or \
-           (event_declined_count is None) or \
-           (event_interested_count is None) or \
-           (event_maybe_count is None) or \
-           (event_noreply_count is None):
+        if (event_start_time is None) or \
+           (event_end_time is None) or \
+           (event_updated_time is None):
             return False
 
-        execute_update_query(conn, event_id, event_attending_count, event_declined_count, event_interested_count,\
-                             event_maybe_count, event_noreply_count)
+        execute_update_query(conn, event_id, event_start_time, event_end_time, event_updated_time)
 
         return True
     except Exception:
-        print ('Error updating event guests for event_id: ' + str(event_id))
+        print ('Error updating event times for event_id: ' + str(event_id))
         tb = traceback.format_exc()
         print(tb)
         return False
