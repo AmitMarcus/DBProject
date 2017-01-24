1f694448c84bea01a64199a239a0fa733cf3a64f
orperel <or_perel@hotmail.com>
2017-01-24 18:57:48 +0200

    fix fb_update

--- build_db/data_from_api/fb_update.py
+++ build_db/data_from_api/fb_update.py
@@ -6,62 +6,51 @@ import traceback
 import MySQLdb
 import facebook
 
-from fb_config import app_token
-from fb_config import database_hostname
+# Facebook app token
+app_token = '190441714759199|QdoMmLvHYEUzSeeLyPK4Awg6Zv4'
 
 
 def fetch_updated_event_guests_data(event_id):
 
     try:
+        event = None
         graph = facebook.GraphAPI(access_token=app_token, version='2.2')
 
         event = graph.get_objects(ids=[event_id],
-                                           fields='id, attending_count,declined_count,maybe_count,' +
-                                                  'noreply_count,interested_count')
+                                           fields='id,noreply_count,interested_count')
+
     except facebook.GraphAPIError:
         print ('Facebook-graph error fetching updated event data. Event_id: ' + str(event_id))
         tb = traceback.format_exc()
         print(tb)
 
     if (event is None) or (len(event) == 0):
-        return None, None, None, None, None
+        return None, None
 
     event_data = event[event_id]
-    event_attending_count = event_data.get('attending_count', None)
-    event_declined_count = event_data.get('declined_count', None)
+
     event_interested_count = event_data.get('interested_count', None)
-    event_maybe_count = event_data.get('maybe_count', None)
     event_noreply_count = event_data.get('noreply_count', None)
 
-    return event_attending_count, event_declined_count, event_interested_count, event_maybe_count, event_noreply_count
+    return event_interested_count, event_noreply_count
 
 
-def execute_update_query(event_id, event_attending_count, event_declined_count, event_interested_count,\
-                         event_maybe_count, event_noreply_count):
+def execute_update_query(conn, event_id, event_interested_count, event_noreply_count):
 
     guests_update_query = 'UPDATE Event_Guests ' \
-                          'SET attending_count=\'%s\', ' \
-                          'declined_count=\'%s\', ' \
-                          'maybe_count=\'%s\', ' \
-                          'interested_count=\'%s\', ' \
+                          'SET interested_count=\'%s\', ' \
                           'noreply_count=\'%s\' ' \
                           'WHERE event_id=\'%s\''
 
-    event_guests_parameters = (event_attending_count,
-                               event_declined_count,
-                               event_maybe_count,
-                               event_interested_count,
+    event_guests_parameters = (event_interested_count,
                                event_noreply_count,
                                long(event_id))
 
     try:
-        con = MySQLdb.connect(database_hostname, 'DbMysql08', 'DbMysql08', 'DbMysql08', charset='utf8')
-
         # Create cursor for executing UPDATE query..
-        cur = con.cursor(MySQLdb.cursors.DictCursor)
+        cur = conn.cursor(MySQLdb.cursors.DictCursor)
 
         cur.execute(guests_update_query, event_guests_parameters)
-        con.commit()
     except MySQLdb.IntegrityError:
         print("Skipping duplicate Event-Guest entry: " + str(event_id))
     except MySQLdb.OperationalError:
@@ -69,28 +58,19 @@ def execute_update_query(event_id, event_attending_count, event_declined_count, 
     except Exception:
         con.rollback()  # Any other exception should be rolled back to protect DB integrity
 
-    con.close()
-
-
-def update_event_guests(event_id):
-    """ Fetches the updated event-guests details using the facebook graph-api (number of attending, declined, etc),
+def update_event_guests(conn, event_id):
+    """ Fetches the updated event-guests interested / no-reply count using the facebook graph-api..
         and updates the event_guests entry at the Event-Guests table in the database accordingly.
         @:return True if the update process succeeded, False is failed
     """
-
     try:
-        event_attending_count, event_declined_count, event_interested_count,\
-        event_maybe_count, event_noreply_count = fetch_updated_event_guests_data(event_id)
+        event_interested_count, event_noreply_count = fetch_updated_event_guests_data(event_id)
 
-        if (event_attending_count is None) or \
-           (event_declined_count is None) or \
-           (event_interested_count is None) or \
-           (event_maybe_count is None) or \
+        if (event_interested_count is None) or \
            (event_noreply_count is None):
             return False
 
-        execute_update_query(event_id, event_attending_count, event_declined_count, event_interested_count,\
-                             event_maybe_count, event_noreply_count)
+        execute_update_query(conn, event_id, event_interested_count, event_noreply_count)
 
         return True
     except Exception:
@@ -98,3 +78,9 @@ def update_event_guests(event_id):
         tb = traceback.format_exc()
         print(tb)
         return False
+
+
+if __name__ == '__main__':
+
+    con = MySQLdb.connect('localhost', 'DbMysql08', 'DbMysql08', 'DbMysql08', charset='utf8')
+    update_event_guests(con, '100101547171558')
--- fb_update.py
+++ fb_update.py
@@ -11,13 +11,18 @@ app_token = '190441714759199|QdoMmLvHYEUzSeeLyPK4Awg6Zv4'
 
 
 def fetch_updated_event_guests_data(event_id):
+    """ Grabs up to date fields from facebook """
 
     try:
         event = None
         graph = facebook.GraphAPI(access_token=app_token, version='2.2')
 
+        # We don't fetch attending count since it's indexed and slow to update (this is a design decision),
+        # and don't fetch declined / maybe since they are meaningless for facebook  public events (always 0).
+        # The shipped version of our app uses only an app key and not a user key (which is required to access
+        # private events that make use of these fields)
         event = graph.get_objects(ids=[event_id],
-                                           fields='id,maybe_count,noreply_count,interested_count')
+                                           fields='id,noreply_count,interested_count')
 
     except facebook.GraphAPIError:
         print ('Facebook-graph error fetching updated event data. Event_id: ' + str(event_id))
@@ -25,27 +30,24 @@ def fetch_updated_event_guests_data(event_id):
         print(tb)
 
     if (event is None) or (len(event) == 0):
-        return None, None, None
+        return None, None
 
     event_data = event[event_id]
 
     event_interested_count = event_data.get('interested_count', None)
-    event_maybe_count = event_data.get('maybe_count', None)
     event_noreply_count = event_data.get('noreply_count', None)
 
-    return event_interested_count, event_maybe_count, event_noreply_count
+    return event_interested_count, event_noreply_count
 
 
-def execute_update_query(conn, event_id, event_interested_count, event_maybe_count, event_noreply_count):
+def execute_update_query(conn, event_id, event_interested_count, event_noreply_count):
 
     guests_update_query = 'UPDATE Event_Guests ' \
-                          'SET maybe_count=\'%s\', ' \
-                          'interested_count=\'%s\', ' \
+                          'SET interested_count=\'%s\', ' \
                           'noreply_count=\'%s\' ' \
                           'WHERE event_id=\'%s\''
 
-    event_guests_parameters = (event_maybe_count,
-                               event_interested_count,
+    event_guests_parameters = (event_interested_count,
                                event_noreply_count,
                                long(event_id))
 
@@ -54,27 +56,32 @@ def execute_update_query(conn, event_id, event_interested_count, event_maybe_cou
         cur = conn.cursor(MySQLdb.cursors.DictCursor)
 
         cur.execute(guests_update_query, event_guests_parameters)
+        # - Autocommit occurs here -
+
     except MySQLdb.IntegrityError:
         print("Skipping duplicate Event-Guest entry: " + str(event_id))
     except MySQLdb.OperationalError:
         pass  # Silently ignore constraint errors, this is the database protecting against bad FB records..
     except Exception:
-        con.rollback()  # Any other exception should be rolled back to protect DB integrity
+        # Any other exception should be rolled back to protect DB integrity
+        # autocommit mode takes care of rollback for us in case of an error, all that remains is to handle
+        # the error..
+        tb = traceback.format_exc()
+        print(tb)
 
 def update_event_guests(conn, event_id):
-    """ Fetches the updated event-guests interested / maybe / no-reply count using the facebook graph-api..
+    """ Fetches the updated event-guests interested / no-reply count using the facebook graph-api..
         and updates the event_guests entry at the Event-Guests table in the database accordingly.
         @:return True if the update process succeeded, False is failed
     """
     try:
-        event_interested_count, event_maybe_count, event_noreply_count = fetch_updated_event_guests_data(event_id)
+        event_interested_count, event_noreply_count = fetch_updated_event_guests_data(event_id)
 
         if (event_interested_count is None) or \
-           (event_maybe_count is None) or \
            (event_noreply_count is None):
             return False
 
-        execute_update_query(conn, event_id, event_interested_count, event_maybe_count, event_noreply_count)
+        execute_update_query(conn, event_id, event_interested_count, event_noreply_count)
 
         return True
     except Exception:
@@ -82,9 +89,3 @@ def update_event_guests(conn, event_id):
         tb = traceback.format_exc()
         print(tb)
         return False
-
-
-if __name__ == '__main__':
-
-    con = MySQLdb.connect('localhost', 'DbMysql08', 'DbMysql08', 'DbMysql08', charset='utf8')
-    update_event_guests(con, '100101547171558')
