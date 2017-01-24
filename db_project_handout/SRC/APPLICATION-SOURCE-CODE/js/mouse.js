5898ac0b6bd83ef5c76e469f73420604e7082968
Amit Marcus <amitmarcus@gmail.com>
2017-01-24 22:07:14 +0200

    Added src to handout

---
+++ db_project_handout/SRC/APPLICATION-SOURCE-CODE/js/mouse.js
@@ -0,0 +1,297 @@
+var mouse = angular.module('mouse', ['mouse.controllers', 'ngRoute', 'ngMap']);
+
+mouse.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/mosaic.html',
+            controller: 'query',
+            resolve: {
+                queryName: function ($route) {
+                    $route.current.params.queryName = "mosaic.complex";
+                }
+            }
+        })
+        .when('/hottest_city/', {
+            templateUrl: 'views/hottest_city.html',
+            controller: 'query',
+            resolve: {
+                queryName: function ($route) {
+                    $route.current.params.queryName = "hottest_city.complex";
+                }
+            }
+        })
+        .when('/hottest_season/', {
+            templateUrl: 'views/hottest_season.html',
+            controller: 'query',
+            resolve: {
+                queryName: function ($route) {
+                    $route.current.params.queryName = "hottest_season.complex";
+                }
+            }
+        })
+        .when('/most_popular_owners/', {
+            templateUrl: 'views/most_popular_owners.html',
+            controller: 'query',
+            resolve: {
+                queryName: function ($route) {
+                    $route.current.params.queryName = "most_popular_owners.complex";
+                }
+            }
+        })
+        .when('/categories_of_most_commented_events/', {
+            templateUrl: 'views/categories_of_most_commented_events.html',
+            controller: 'query',
+            resolve: {
+                queryName: function ($route) {
+                    $route.current.params.queryName = "categories_of_most_commented_events.complex";
+                }
+            }
+        })
+        .when('/most_sentimental_owners/', {
+            templateUrl: 'views/most_sentimental_owners.html',
+            controller: 'query',
+            resolve: {
+                queryName: function ($route) {
+                    $route.current.params.queryName = "most_sentimental_owners.complex";
+                }
+            }
+        })
+        .when('/count_events_by_city/', {
+            templateUrl: 'views/count_events_by_city.html',
+            controller: 'countEventsByCity',
+            resolve: {
+                queryName: function ($route) {
+                    $route.current.params.queryName = "count_events_by_city.complex";
+                }
+            }
+        })
+        .when('/top_places_of_top_category/', {
+            templateUrl: 'views/top_places_of_top_category.html',
+            controller: 'query',
+            resolve: {
+                queryName: function ($route) {
+                    $route.current.params.queryName = "top_places_of_top_category.complex";
+                }
+            }
+        })
+        .when('/best_streets_USA_UK/', {
+            templateUrl: 'views/best_streets_USA_UK.html',
+            controller: 'query',
+            resolve: {
+                queryName: function ($route) {
+                    $route.current.params.queryName = "best_streets_USA_UK.complex";
+                }
+            }
+        })
+        .when('/event/', {
+            templateUrl: 'views/event.html',
+            controller: 'event'
+        })
+        .when('/search/', {
+            templateUrl: 'views/search.html',
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    $locationProvider.html5Mode(true);
+});
+
+mouse.factory('ServerService', ['$location', function ($location) {
+    return {
+        address: 'http://' + $location.host() + ':' + $location.port()
+    };
+}]);
+
+angular.module('mouse.controllers', [])
+    .controller('main', function ($scope, $http, $location, ServerService) {
+        $http.get(ServerService.address + '/api/query/cities.simple/')
+            .then(function (response) {
+                console.log(response);
+                $scope.cities = response.data;
+            }, function errorCallback(response) {
+                alert("There is a problem with the server. Try again later.");
+            });
+    
+        $http.get(ServerService.address + '/api/query/message.simple/')
+            .then(function (response) {
+                console.log(response);
+                $scope.message = response.data[0];
+            }, function errorCallback(response) {
+                alert("There is a problem with the server. Try again later.");
+            });
+    
+        $scope.searchString = "";
+
+        $scope.search = function () {
+            console.log("Search...");
+            var data = {};
+            data.searchString = $scope.searchString;
+
+            $http.post(ServerService.address + '/api/search/', data)
+                .then(function (response) {
+                    console.log(response);
+                    $scope.searchResults = response.data;
+                    $location.path("/search");
+                }, function errorCallback(response) {
+                    alert("There is a problem with the server. Try again later.");
+                });
+        }
+        
+        $scope.data = {};
+        $scope.sendMsg = function (data) {
+            if (!("message" in $scope.data) || $scope.data.message == "") {
+                $scope.errorMsg = "No message in new message!";
+                $('#error').modal();
+                return;
+            }
+            if (!("fullname" in $scope.data) || $scope.data.fullname == "") {
+                $scope.errorMsg = "No fullname in new message!";
+                $('#error').modal();
+                return;
+            }
+            if (!("city_id" in $scope.data) || $scope.data.city_id == "") {
+                $scope.errorMsg = "No city in new message!";
+                $('#error').modal();
+                return;
+            }
+            console.log("Add msg..");
+            $http.post(ServerService.address + '/api/message/add/', $scope.data)
+                .then(function (response) {
+                    $http.get(ServerService.address + '/api/query/message.simple/')
+                        .then(function (response) {
+                            console.log(response);
+                            $scope.message = response.data[0];
+                            $scope.data = {};
+                            $('#addMsgNotify').modal();
+                        }, function errorCallback(response) {
+                            alert("There is a problem with the server. Try again later.");
+                        });
+                }, function errorCallback(response) {
+                    alert("There is a problem with the server. Try again later.");
+                });
+        }
+
+        $scope.getMapStyles = function () {
+            return [{"featureType":"all","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"color":"#e5c163"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#c4c4c4"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#e5c163"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21},{"visibility":"on"}]},{"featureType":"poi.business","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#e5c163"},{"lightness":"0"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"labels.text.stroke","stylers":[{"color":"#e5c163"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#575757"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.stroke","stylers":[{"color":"#2c2c2c"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#999999"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
+        }
+    })
+    .controller('event', function ($scope, $http, $routeParams, ServerService) {
+        $scope.ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        var eventId = $routeParams.id;
+        $http.get(ServerService.address + '/api/event/' + eventId + '/')
+            .then(function (response) {
+                console.log(response);
+                $scope.event = response.data;
+            
+                if ($scope.event.latitude != null) {
+                    var data = {};
+                    data.latitude = $scope.event.latitude
+                    data.longitude = $scope.event.longitude
+
+                    $http.post(ServerService.address + '/api/nearby_events_by_coordinates/', data)
+                        .then(function (response) {
+                            console.log(response);
+                            $scope.nearby = response.data;
+                        }, function errorCallback(response) {
+                            alert("There is a problem with the server. Try again later.");
+                        });                   
+                }
+            }, function errorCallback(response) {
+                alert("There is a problem with the server. Try again later.");
+            });
+
+        $http.get(ServerService.address + '/api/event/' + eventId + '/comments/')
+            .then(function (response) {
+                console.log(response);
+                $scope.comments = response.data;
+            });
+
+        $scope.inUpdateProcess = false;
+        $scope.updateButtonCaption = "Update Counters";
+        $scope.updateCounters = function () {
+            if ($scope.inUpdateProcess) {
+                return;
+            }
+            
+            $scope.inUpdateProcess = true;
+            $scope.updateButtonCaption = "Please wait...";
+            
+            $http.get(ServerService.address + '/api/event/' + eventId + '/update/')
+                .then(function (response) {
+                    $http.get(ServerService.address + '/api/event/' + eventId + '/')
+                        .then(function (response) {
+                            console.log(response);
+                            $scope.event = response.data;
+                            $scope.inUpdateProcess = false;
+                            $scope.updateButtonCaption = "Update Counters";
+
+                            $('#updateNotify').modal();
+                        }, function errorCallback(response) {
+                            alert("There is a problem with the server. Try again later.");
+                        });
+                }, function errorCallback(response) {
+                    alert("There is a problem with the server. Try again later.");
+                });
+        }
+        
+        $scope.isInUpdateProcess = function () {
+            return $scope.inUpdateProcess;
+        }
+
+        $scope.sendComment = function () {
+            var data = {};
+            data.newComment = $scope.newComment;
+
+            $http.post(ServerService.address + '/api/event/' + eventId + '/comments/add/', data)
+                .then(function (response) {
+                    console.log(response);
+                    $http.get(ServerService.address + '/api/event/' + eventId + '/comments/')
+                        .then(function (response) {
+                            console.log(response);
+                            $scope.comments = response.data;
+                        
+                            $scope.newComment = "";
+
+                            $('#addCommentNotify').modal();
+                        }, function errorCallback(response) {
+                            alert("There is a problem with the server. Try again later.");
+                        });
+                }, function errorCallback(response) {
+                    alert("There is a problem with the server. Try again later.");
+                });
+        }
+
+        $scope.buyTickets = function (path) {
+            if (path != null) {
+                window.open(path, '_blank');
+            }
+        };
+    })
+    .controller('query', function ($scope, $http, ServerService, $routeParams) {
+        $http.get(ServerService.address + '/api/query/' + $routeParams.queryName + '/')
+            .then(function (response) {
+                console.log(response);
+                $scope.data = response.data;
+            }, function errorCallback(response) {
+                alert("There is a problem with the server. Try again later.");
+            });
+    })
+    .controller('countEventsByCity', function ($scope, $http, ServerService, $routeParams) {
+        $http.get(ServerService.address + '/api/query/cities.simple/')
+            .then(function (response) {
+                console.log(response);
+                $scope.cities = response.data;
+            }, function errorCallback(response) {
+                alert("There is a problem with the server. Try again later.");
+            });
+    
+        $scope.chooseCity = function() {
+           $http.get(ServerService.address + '/api/count_events_by_city/' + $scope.chosenCityId + '/')
+            .then(function (response) {
+                console.log(response);
+                $scope.chosenCity = response.data;
+            }, function errorCallback(response) {
+                alert("There is a problem with the server. Try again later.");
+            });        
+        }
+    });;
---
+++ db_project_handout/SRC/APPLICATION-SOURCE-CODE/queries/hottest_season.complex.sql
@@ -0,0 +1,60 @@
+# -- All events in hottest season in the city of the biggest event --
+# Returns all events ordered by number of attendents in the city that hosts the event
+# with the largest amount of attending people, and the city hosts at least 10 events total.
+# The events returned must also occur within the same season as the event
+# with the maximal amount of attendees
+# (meaning 2 months before or after the maximal event, can occur on different years).
+SELECT 
+    Event.id AS event_id,
+    Event.name AS event_name,
+    Category.name AS event_category,
+    City.name AS city_name,
+    Event_Desc_Search.description AS event_description,
+    DATE_FORMAT(Event_Time.start_time, '%e/%c/%Y %H:%i') AS event_start_time,
+    DATE_FORMAT(Event_Time.end_time, '%e/%c/%Y %H:%i') AS event_end_time,
+    Event_Guests.attending_count AS event_attending_count
+FROM
+    Event,
+    Category,
+    Place,
+    Event_Place,
+    Street,
+    City,
+    Event_Time,
+    Event_Guests,
+    Event_Desc_Search,
+    (SELECT 
+        MONTH(EventTime1.start_time) AS monthOfHighestAtt,
+            EventfulCities.id AS cityID,
+            EventGuests1.attending_count AS maxAtt
+    FROM
+        Event AS Event1, Place AS Place1, Event_Place AS EventPlace1, Street AS Street1, Event_Time AS EventTime1, Event_Guests AS EventGuests1, (SELECT 
+        City.id
+    FROM
+        City
+    JOIN Street ON Street.city_id = City.id
+    JOIN Place ON Street.id = Place.street_id
+    JOIN Event_Place ON Event_Place.place_id = Place.id
+    GROUP BY City.id
+    HAVING COUNT(Event_Place.event_id) >= 10) AS EventfulCities
+    WHERE
+        Event1.id = EventTime1.event_id
+            AND Event1.id = EventGuests1.event_id
+            AND Event1.id = EventPlace1.event_id
+            AND EventPlace1.place_id = Place1.id
+            AND Place1.street_id = Street1.id
+            AND Street1.city_id = EventfulCities.id
+    ORDER BY (maxAtt) DESC
+    LIMIT 1) AS maxAttTable
+WHERE
+    Event.category_id = Category.id
+        AND Event.id = Event_Place.event_id
+        AND Event_Place.place_id = Place.id
+        AND Place.street_id = Street.id
+        AND Street.city_id = City.id
+        AND City.id = maxAttTable.cityID
+        AND Event.id = Event_Time.event_id
+        AND Event_Guests.event_id = Event.id
+        AND Event.id = Event_Desc_Search.event_id
+        AND ABS(MONTH(Event_Time.start_time) - maxAttTable.monthOfHighestAtt) <= 2
+ORDER BY Event_Guests.attending_count DESC;
