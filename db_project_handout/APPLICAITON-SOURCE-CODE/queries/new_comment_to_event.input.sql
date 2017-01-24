74df4a7b4aa3f59b67f89e626869f545767e6e61
Amit Marcus <amitmarcus@gmail.com>
2017-01-24 22:04:58 +0200

    - Delete src from handout

--- db_project_handout/APPLICAITON-SOURCE-CODE/queries/new_comment_to_event.input.sql
+++
@@ -1,5 +0,0 @@
-#Insert new comment according to message and event_id as inputs into our Comment table
-
-
-
-INSERT INTO Comment (message, event_id) VALUES (%s, %s)
--- db_project_handout/APPLICAITON-SOURCE-CODE/queries/new_message.input.sql
+++
@@ -1,3 +0,0 @@
-#Insert new message according to message,fullname and city_id as inputs into our GlobalMessage table.
-
-INSERT INTO GlobalMessage (message, fullname, city_id) VALUES (%s, %s, %s)
